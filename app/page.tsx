84295337f5a502ff3c8dbd8c57d826de9b84760b
anoopraju31 <anoopraju31@gmail.com>
2024-07-23 23:03:19 +0530

    Home Page - Prefooter Section - styled

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import PreFooter from './sections/prefooter'
 const Home: FC = () => {
 	return (
 		<main className='bg-black min-h-screen'>
+			<div className='h-screen'></div>
 			<PreFooter />
 		</main>
 	)
--- app/sections/prefooter/index.tsx
+++ app/sections/prefooter/index.tsx
@@ -1,8 +1,36 @@
 import { type FC } from 'react'
 import styles from './styles.module.css'
+import Link from 'next/link'
 
 const PreFooter: FC = () => {
-	return <div>PreFooter</div>
+	return (
+		<section className={styles.prefooter}>
+			<div className={styles.layout__container}>
+				<div className={styles.grid__container}>
+					<div className=''>
+						<h3 className={styles.heading}>
+							Plan the present.
+							<br />
+							Build the future.
+						</h3>
+					</div>
+
+					<div className={styles.links__outter__container}>
+						<div className={styles.links__inner__container}>
+							<Link className={styles.get__started__link} href='/'>
+								{' '}
+								Get Started{' '}
+							</Link>
+							<Link className={styles.talk__to__sales} href='/'>
+								{' '}
+								Talk to sales{' '}
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
 }
 
 export default PreFooter
