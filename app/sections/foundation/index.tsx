4f04394a089d349f1de459d8a2b391c51781e060
anoopraju31 <anoopraju31@gmail.com>
2024-07-24 00:12:42 +0530

    Section Heading Component - initial

--- app/sections/foundation/index.tsx
+++ app/sections/foundation/index.tsx
@@ -1,11 +1,13 @@
 import { type FC } from 'react'
 import UnderTheHood from './underToHood.svg'
 import styles from './styles.module.css'
+import SectionHeading from '@/components/sectionHeading'
 
 const Foundation: FC = () => {
 	return (
 		<section className={styles.foundation}>
 			<div className={styles.container}>
+				<SectionHeading />
 				<div className='h-screen w-5/12 bg-orange-400'></div>
 				<div className={styles.under__the__hood}>
 					<UnderTheHood />
---
+++ components/sectionHeading/index.tsx
@@ -0,0 +1,7 @@
+import { type FC } from 'react'
+
+const SectionHeading: FC = () => {
+	return <div>SectionHeading</div>
+}
+
+export default SectionHeading
