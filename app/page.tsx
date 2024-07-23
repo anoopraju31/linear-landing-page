cdf8b960578fe000aa1b9aa0ffb4dad2002c8bf9
anoopraju31 <anoopraju31@gmail.com>
2024-07-23 23:34:54 +0530

    Home Page - Foundation Section - initial

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { type FC } from 'react'
 import PreFooter from './sections/prefooter'
+import Foundation from './sections/foundation'
 
 const Home: FC = () => {
 	return (
 		<main className='bg-black min-h-screen'>
 			<div className='h-screen'></div>
+			<Foundation />
 			<PreFooter />
 		</main>
 	)
---
+++ app/sections/foundation/index.tsx
@@ -0,0 +1,8 @@
+import { type FC } from 'react'
+import styles from './styles.module.css'
+
+const Foundation: FC = () => {
+	return <section className={styles.foundation}>Foundation</section>
+}
+
+export default Foundation
