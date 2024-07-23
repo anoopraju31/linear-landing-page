3ba7abac873ee6a6725ff5f26ea252bfc222dfc5
anoopraju31 <anoopraju31@gmail.com>
2024-07-24 00:00:25 +0530

    Home Page - Foundation Section - under to hood svg added

--- app/sections/foundation/index.tsx
+++ app/sections/foundation/index.tsx
@@ -1,8 +1,18 @@
 import { type FC } from 'react'
+import UnderTheHood from './underToHood.svg'
 import styles from './styles.module.css'
 
 const Foundation: FC = () => {
-	return <section className={styles.foundation}>Foundation</section>
+	return (
+		<section className={styles.foundation}>
+			<div className={styles.container}>
+				<div className='h-screen w-5/12 bg-orange-400'></div>
+				<div className={styles.under__the__hood}>
+					<UnderTheHood />
+				</div>
+			</div>
+		</section>
+	)
 }
 
 export default Foundation
