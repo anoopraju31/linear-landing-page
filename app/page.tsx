f65c91a107c87980e49a66f2a071538a0a89bf12
anoopraju31 <anoopraju31@gmail.com>
2024-07-24 15:12:55 +0530

    Home Page - Issue Tracking Section - initial

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import { type FC } from 'react'
 import PreFooter from './sections/prefooter'
 import Foundation from './sections/foundation'
 import Collaborate from './sections/collaborate'
+import IssueTracking from './sections/issue-tracking'
 
 const Home: FC = () => {
 	return (
 		<main className=' min-h-screen'>
+			<IssueTracking />
 			<Collaborate />
 			<Foundation />
 			<PreFooter />
---
+++ app/sections/issue-tracking/index.tsx
@@ -0,0 +1,8 @@
+import { type FC } from 'react'
+import styles from './styles.module.css'
+
+const IssueTracking: FC = () => {
+	return <div>IssueTracking</div>
+}
+
+export default IssueTracking
