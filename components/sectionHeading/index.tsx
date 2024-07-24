59f3a4dbe2da304a4dc5bbfcd128f6f59feb0af6
anoopraju31 <anoopraju31@gmail.com>
2024-07-24 10:52:01 +0530

    Section Heading Component styled

--- components/sectionHeading/index.tsx
+++ components/sectionHeading/index.tsx
@@ -1,7 +1,25 @@
 import { type FC } from 'react'
+import styles from './styles.module.css'
+import { cn } from '@/lib/utils'
 
-const SectionHeading: FC = () => {
-	return <div>SectionHeading</div>
+type Props = {
+	badgeText: string
+	badgeStyle?: string
+	heading: string
+}
+
+const SectionHeading: FC<Props> = ({ badgeStyle, badgeText, heading }) => {
+	return (
+		<div>
+			<div className={styles.badge__container}>
+				<div className={cn(styles.badge, badgeStyle)}></div>
+				{/*  */}
+				<span className={styles.badge__text}> {badgeText} </span>
+			</div>
+
+			<h2 className={styles.heading}> {heading} </h2>
+		</div>
+	)
 }
 
 export default SectionHeading
