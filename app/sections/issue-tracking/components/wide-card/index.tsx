fa217a32c063dbe5e5764b561850620be44a66ff
anoopraju31 <anoopraju31@gmail.com>
2024-07-24 20:59:31 +0530

    Home Page - Issue Tracking Section - Wide Card Component - initial

---
+++ app/sections/issue-tracking/components/wide-card/index.tsx
@@ -0,0 +1,8 @@
+import { type FC } from 'react'
+import styles from './styles.module.css'
+
+const WideCard: FC = () => {
+	return <div className={styles.wide__card}>WideCard</div>
+}
+
+export default WideCard
