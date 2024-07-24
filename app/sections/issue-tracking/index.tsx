88907fd0b15d427fc03c4e431a8a315b21bf7c8a
anoopraju31 <anoopraju31@gmail.com>
2024-07-24 15:31:54 +0530

    Home Page - Issue Tracking Section - heading added

--- app/sections/issue-tracking/index.tsx
+++ app/sections/issue-tracking/index.tsx
@@ -1,8 +1,31 @@
 import { type FC } from 'react'
 import styles from './styles.module.css'
+import SectionHeading from '@/components/sectionHeading'
 
 const IssueTracking: FC = () => {
-	return <div>IssueTracking</div>
+	return (
+		<section className={styles.issue__tracking}>
+			<div className={styles.container}>
+				<div className={styles.heading__container}>
+					<div className={styles.heading__inner__container}>
+						<SectionHeading
+							heading='Issue tracking you’ll enjoy using'
+							badgeText='Task tracking and sprint planning'
+							badgeStyle='bg-[#D4B144] border-none'
+						/>
+
+						<div>
+							<p>
+								<span>Optimized for speed and efficiency.</span> Create tasks in
+								seconds, discuss issues in context, and breeze through your work
+								in views tailored to you and your team.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
 }
 
 export default IssueTracking
