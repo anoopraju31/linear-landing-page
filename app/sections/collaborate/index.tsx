e735cd78a57cf2c20d6b5525b1bb04d736ce8e14
anoopraju31 <anoopraju31@gmail.com>
2024-07-24 13:22:54 +0530

    Home Page - Collaborate Section - heading styled

--- app/sections/collaborate/index.tsx
+++ app/sections/collaborate/index.tsx
@@ -1,7 +1,31 @@
 import { type FC } from 'react'
+import styles from './styles.module.css'
+import SectionHeading from '@/components/sectionHeading'
 
 const Collaborate: FC = () => {
-	return <div>Collaborate</div>
+	return (
+		<section className={styles.collaborate}>
+			<div className={styles.container}>
+				<div className={styles.heading__container}>
+					<div className={styles.heading__inner__container}>
+						<SectionHeading
+							heading='Collaborate across tools and teams'
+							badgeText='Workflows and integrations'
+							badgeStyle='bg-[#b59aff] border-none'
+						/>
+					</div>
+
+					<div className={styles.heading__text__container}>
+						<p>
+							Expand the capabilities of the Linear system with a wide variety
+							of integrations that keep everyone in your organization aligned
+							and focused.
+						</p>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
 }
 
 export default Collaborate
