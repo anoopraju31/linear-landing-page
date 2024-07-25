b95ab85f9cf4a8e9d559eb2d49b3e1cb62107a02
anoopraju31 <anoopraju31@gmail.com>
2024-07-26 01:09:12 +0530

    Home Page - Customers Section  - customer list grid - styled

---
+++ app/sections/customers/components/customer-list/grid-cell.tsx
@@ -0,0 +1,87 @@
+'use client'
+
+import { ReactNode, type FC } from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
+import { cn } from '@/lib/utils'
+import styles from './styles.module.css'
+
+type Props = {
+	layer: number
+	icon1: ReactNode
+	icon2: ReactNode
+	icon3: ReactNode
+}
+
+const popIn = {
+	initial: {
+		opacity: 0,
+		scale: 0.8,
+		filter: 'blur(5px)',
+	},
+	animate: {
+		opacity: 1,
+		scale: 1,
+		filter: 'blur(0)',
+		transition: {
+			type: 'spring',
+			duration: 1,
+			scale: { duration: 0.8 },
+			filter: { duration: 0.4 },
+		},
+	},
+	exit: {
+		opacity: 0,
+		scale: 0.8,
+		filter: 'blur(5px)',
+		transition: {
+			ease: 'easeIn',
+			duration: 0.7,
+			scale: { duration: 0.7 },
+			filter: { duration: 0.3, delay: 0.4 },
+		},
+	},
+}
+
+const GridCell: FC<Props> = ({ layer, icon1, icon2, icon3 }) => {
+	return (
+		<div className={styles.grid__item}>
+			<AnimatePresence>
+				{layer === 1 && (
+					<motion.div
+						variants={popIn}
+						initial='initial'
+						animate='animate'
+						exit='exit'
+						className={cn(styles.item)}>
+						{icon1}
+					</motion.div>
+				)}
+			</AnimatePresence>
+			<AnimatePresence>
+				{layer === 2 && (
+					<motion.div
+						variants={popIn}
+						initial='initial'
+						animate='animate'
+						exit='exit'
+						className={cn(styles.item)}>
+						{icon2}
+					</motion.div>
+				)}
+			</AnimatePresence>
+			<AnimatePresence>
+				{layer === 3 && (
+					<motion.div
+						variants={popIn}
+						initial='initial'
+						animate='animate'
+						exit='exit'
+						className={cn(styles.item)}>
+						{icon3}
+					</motion.div>
+				)}
+			</AnimatePresence>
+		</div>
+	)
+}
+export default GridCell
--- app/sections/customers/components/customer-list/index.tsx
+++ app/sections/customers/components/customer-list/index.tsx
@@ -1,8 +1,79 @@
-import { type FC } from 'react'
+'use client'
+
+import { useEffect, useState, type FC } from 'react'
+import GridCell from './grid-cell'
+import Logo1 from '@/assets/logo-1.svg'
+import Logo2 from '@/assets/logo-2.svg'
+import Logo3 from '@/assets/logo-3.svg'
+import Logo4 from '@/assets/logo-4.svg'
+import Logo5 from '@/assets/logo-5.svg'
+import Logo6 from '@/assets/logo-6.svg'
+import Logo7 from '@/assets/logo-7.svg'
+import Logo8 from '@/assets/logo-8.svg'
+import Logo9 from '@/assets/logo-9.svg'
+import Logo10 from '@/assets/logo-10.svg'
+import Logo11 from '@/assets/logo-11.svg'
+import Logo12 from '@/assets/logo-12.svg'
+import Logo13 from '@/assets/logo-13.svg'
+import Logo14 from '@/assets/logo-14.svg'
+import Logo15 from '@/assets/logo-15.svg'
+import Logo16 from '@/assets/logo-16.svg'
+import Logo17 from '@/assets/logo-17.svg'
+import Logo18 from '@/assets/logo-18.svg'
 import styles from './styles.module.css'
 
 const CustomerList: FC = () => {
-	return <div>CustomerList</div>
+	const [layer, setLayer] = useState<number>(1)
+
+	useEffect(() => {
+		const changeLayer = () => setLayer((prev) => (prev % 3) + 1)
+		const interval = setInterval(changeLayer, 3000)
+
+		return () => clearInterval(interval)
+	}, [])
+
+	return (
+		<div className={styles.customer__grid}>
+			<div className={styles.logo__grid}>
+				<GridCell
+					layer={layer}
+					icon1={<Logo1 />}
+					icon2={<Logo2 />}
+					icon3={<Logo3 />}
+				/>
+				<GridCell
+					layer={layer}
+					icon1={<Logo4 />}
+					icon2={<Logo5 />}
+					icon3={<Logo6 />}
+				/>
+				<GridCell
+					layer={layer}
+					icon1={<Logo7 />}
+					icon2={<Logo8 />}
+					icon3={<Logo9 />}
+				/>
+				<GridCell
+					layer={layer}
+					icon1={<Logo10 />}
+					icon2={<Logo11 />}
+					icon3={<Logo12 />}
+				/>
+				<GridCell
+					layer={layer}
+					icon1={<Logo13 />}
+					icon2={<Logo14 />}
+					icon3={<Logo15 />}
+				/>
+				<GridCell
+					layer={layer}
+					icon1={<Logo16 />}
+					icon2={<Logo17 />}
+					icon3={<Logo18 />}
+				/>
+			</div>
+		</div>
+	)
 }
 
 export default CustomerList
