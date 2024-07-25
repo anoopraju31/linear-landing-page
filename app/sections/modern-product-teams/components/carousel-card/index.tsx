9f18f9a44b51fe730c7155f882b7e05edde35d4e
anoopraju31 <anoopraju31@gmail.com>
2024-07-25 22:50:15 +0530

    Home Page - Modern Product Teams Section  - Carousel card Component added

--- app/sections/modern-product-teams/components/carousel-card/index.tsx
+++ app/sections/modern-product-teams/components/carousel-card/index.tsx
@@ -3,22 +3,22 @@ import Image from 'next/image'
 import styles from './styles.module.css'
 import { Plus } from 'lucide-react'
 
-const CarouselCard: FC = () => {
+export type CarouselCardProps = {
+	id: string
+	img: string
+	title: string
+}
+
+const CarouselCard: FC<CarouselCardProps> = ({ img, title }) => {
 	return (
 		<div className={styles.carousel__card}>
 			<button className={styles.outter__container}>
 				<div className={styles.img__container}>
-					<Image
-						src='/perfection.avif'
-						alt=''
-						width={960}
-						height={914}
-						loading='lazy'
-					/>
+					<Image src={img} alt='' width={960} height={914} />
 				</div>
 				<div className={styles.text__container}>
 					<div className={styles.title}>
-						<span> Purpose-built for product development </span>
+						<span> {title} </span>
 					</div>
 
 					<div className={styles.icon__container}>
--- app/sections/modern-product-teams/components/carousel/index.tsx
+++ app/sections/modern-product-teams/components/carousel/index.tsx
@@ -1,15 +1,16 @@
 import { type FC } from 'react'
 import styles from './styles.module.css'
 import CarouselCard from '../carousel-card'
+import { modernProductCards } from '@/lib/constant'
 
 const Carousel: FC = () => {
 	return (
 		<div>
 			<div className={styles.carousel__container}>
 				<div className={styles.carousel__inner__container}>
-					<CarouselCard />
-					<CarouselCard />
-					<CarouselCard />
+					{modernProductCards.map((card) => (
+						<CarouselCard key={card.id} {...card} />
+					))}
 				</div>
 			</div>
 		</div>
