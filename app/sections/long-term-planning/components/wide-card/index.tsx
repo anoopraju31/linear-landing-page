941367f54a05c3984509bd63f63cc2f607308daa
anoopraju31 <anoopraju31@gmail.com>
2024-07-25 16:48:51 +0530

    Home Page - Long Term Planning Section  - wide Card - Tab functionality added

--- app/sections/long-term-planning/components/wide-card/index.tsx
+++ app/sections/long-term-planning/components/wide-card/index.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+'use client'
 
-const WideCard = () => {
-	return <div>WideCard</div>
+import { useState, type FC } from 'react'
+import TabHeader from './tab-header'
+import TabBody from './tab-body'
+import styles from './styles.module.css'
+import { tabHeaders } from '../../feature-lookup-data'
+
+const WideCard: FC = () => {
+	const [currentTab, setCurrentTab] = useState<string>(tabHeaders[0].id)
+	return (
+		<div className={styles.wide__card__tab}>
+			<TabHeader
+				tabHeaders={tabHeaders}
+				currentTab={currentTab}
+				changeTab={setCurrentTab}
+			/>
+			<TabBody currentTab={currentTab} />
+		</div>
+	)
 }
 
 export default WideCard
