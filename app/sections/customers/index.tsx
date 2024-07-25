f83d95ce586b2f46d80fe624e9b0c02d2fdbfb01
anoopraju31 <anoopraju31@gmail.com>
2024-07-25 23:46:12 +0530

    Home Page - Customers Section  - heading added

--- app/sections/customers/index.tsx
+++ app/sections/customers/index.tsx
@@ -1,8 +1,26 @@
 import { type FC } from 'react'
 import styles from './styles.module.css'
+import LayoutWrapper from '@/components/layout-wrapper'
 
 const Customers: FC = () => {
-	return <div>Customers</div>
+	return (
+		<section className={styles.customers}>
+			<LayoutWrapper>
+				<p className={styles.description__large__screen}>
+					<span className={styles.highlight}>
+						Powering the world&lsquo;s best product teams.
+					</span>
+					<br />
+					From next-gen startups to established enterprises.
+				</p>
+
+				<p className={styles.description__small__screen}>
+					Powering the world&lsquo;s best product teams. From next-gen startups
+					to established enterprises.
+				</p>
+			</LayoutWrapper>
+		</section>
+	)
 }
 
 export default Customers
