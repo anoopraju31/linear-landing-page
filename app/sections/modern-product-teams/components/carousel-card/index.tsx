a04e13b38f0de4d4e3a09889c8e55e77f6a94395
anoopraju31 <anoopraju31@gmail.com>
2024-07-25 21:22:10 +0530

    Home Page - Modern Product Teams Section  - Carousel Component - layout added

---
+++ app/sections/modern-product-teams/components/carousel-card/index.tsx
@@ -0,0 +1,10 @@
+import { type FC } from 'react'
+import styles from './styles.module.css'
+
+const CarouselCard: FC = () => {
+	return (
+		<div className='h-[360px] aspect-[336/360] bg-slate-400 rounded-xl'></div>
+	)
+}
+
+export default CarouselCard
--- app/sections/modern-product-teams/components/carousel/index.tsx
+++ app/sections/modern-product-teams/components/carousel/index.tsx
@@ -1,8 +1,19 @@
 import { type FC } from 'react'
 import styles from './styles.module.css'
+import CarouselCard from '../carousel-card'
 
 const Carousel: FC = () => {
-	return <div>Carousel</div>
+	return (
+		<div>
+			<div className={styles.carousel__container}>
+				<div className={styles.carousel__inner__container}>
+					<CarouselCard />
+					<CarouselCard />
+					<CarouselCard />
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Carousel
