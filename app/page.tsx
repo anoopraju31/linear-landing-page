e6ccc2508e7ef38c6a9afc845e79dce8b94678ee
anoopraju31 <anoopraju31@gmail.com>
2024-07-25 23:32:56 +0530

    Home Page - Customers Section  - initial

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import { type FC } from 'react'
+import Customers from './sections/customers'
+import ModernProductTeams from './sections/modern-product-teams'
 import LongTermPlanning from './sections/long-term-planning'
 import IssueTracking from './sections/issue-tracking'
 import Collaborate from './sections/collaborate'
 import Foundation from './sections/foundation'
 import PreFooter from './sections/prefooter'
-import ModernProductTeams from './sections/modern-product-teams'
 
 const Home: FC = () => {
 	return (
 		<main className=' min-h-screen'>
+			<Customers />
 			<ModernProductTeams />
 			<LongTermPlanning />
 			<IssueTracking />
---
+++ app/sections/customers/index.tsx
@@ -0,0 +1,8 @@
+import { type FC } from 'react'
+import styles from './styles.module.css'
+
+const Customers: FC = () => {
+	return <div>Customers</div>
+}
+
+export default Customers
