25e77e7e505e669437f2f2f9f49809b02bc164fd
anoopraju31 <anoopraju31@gmail.com>
2024-07-25 22:20:41 +0530

    Home Page - Modern Product Teams Section  - Carousel card Component added

--- app/sections/modern-product-teams/components/carousel-card/index.tsx
+++ app/sections/modern-product-teams/components/carousel-card/index.tsx
@@ -1,9 +1,32 @@
 import { type FC } from 'react'
+import Image from 'next/image'
 import styles from './styles.module.css'
+import { Plus } from 'lucide-react'
 
 const CarouselCard: FC = () => {
 	return (
-		<div className='h-[360px] aspect-[336/360] bg-slate-400 rounded-xl'></div>
+		<div className={styles.carousel__card}>
+			<button className={styles.outter__container}>
+				<div className={styles.img__container}>
+					<Image
+						src='/perfection.avif'
+						alt=''
+						width={960}
+						height={914}
+						loading='lazy'
+					/>
+				</div>
+				<div className={styles.text__container}>
+					<div className={styles.title}>
+						<span> Purpose-built for product development </span>
+					</div>
+
+					<div className={styles.icon__container}>
+						<Plus />
+					</div>
+				</div>
+			</button>
+		</div>
 	)
 }
 
